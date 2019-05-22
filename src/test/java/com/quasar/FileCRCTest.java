5ad533c581fed84bbb733f435a867b15d3e85fbf
michal <michal.rorat@tantalumcorporation.com>
2019-05-22 19:23:42 +0100

    CRC calculations test

---
+++ src/test/java/com/quasar/FileCRCTest.java
@@ -0,0 +1,14 @@
+package com.quasar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileCRCTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
